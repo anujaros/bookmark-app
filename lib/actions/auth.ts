2c7aa5d891cd2b0f30ef64067755e1d71b2f7f65
anuja <anujarosebaby@gmail.com>
2026-02-14 02:07:30 +0530

    google-oauth

---
+++ lib/actions/auth.ts
@@ -0,0 +1,38 @@
+'use server'
+
+import { redirect } from "next/navigation"
+import { createClientForServer } from "../supabase/server"
+import { Provider } from "@supabase/supabase-js"
+
+const signInWith = (provider: Provider) => async() => {
+    const supabase = await createClientForServer()
+
+    const auth_callback_url = `${process.env.SITE_URL}/auth/callback`
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider,
+        options: {
+            redirectTo: auth_callback_url,
+        },
+    })
+
+     if (error) {
+        console.error(error);
+        throw new Error("OAuth sign-in failed")
+    }
+
+    if (!data?.url) {
+        throw new Error("No redirect URL returned from Supabase")
+    }
+    redirect(data.url)
+}
+
+const signInWithGoogle = signInWith('google')
+
+const signOut = async() => {
+    const supabase = await createClientForServer()
+    await supabase.auth.signOut()
+    redirect("/")
+}
+
+export { signInWithGoogle, signOut}
